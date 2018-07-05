cfb3321e61dadd9bd57d394db07c740c613a1be5
alexandre.olavio <alexandre.olavio@m2msolutions.com.br>
2018-07-05 18:31:42 -0300

    Teste do componente AlbumList

--- src/test/AlbumList.spec.js
+++ src/test/AlbumList.spec.js
@@ -1,6 +1,127 @@
-/*
-'AlbumList'
-  -'should exist'
-  - 'should create and append the markup given a correct data'
-  - 'should create and append when more than 1 album'
-*/
+import 'jsdom-global/register';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import AlbumInfoView from '../app/js/views/AlbumListView';
+import Helper from '../app/js/helpers/Helper';
+
+describe('AlbumList', () => {
+  it('should exist', () => {
+    expect(AlbumInfoView).to.exist;
+  });
+
+  const data = [
+    {
+      album_type: 'album',
+      artists: [{
+        name: 'Incubus',
+      }],
+      id: '6peEdPVO73WtgGah5sEhX4',
+      images: [{
+        height: 640,
+        url: 'https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757',
+        width: 640,
+      }, {
+        height: 300,
+        url: 'https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6',
+        width: 300,
+      }, {
+        height: 64,
+        url: 'https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16',
+        width: 64,
+      }],
+      name: 'The Essential Incubus',
+      type: 'album',
+    },
+  ];
+
+  const data2 = [
+    {
+      album_type: 'album',
+      artists: [{
+        name: 'Incubus',
+      }],
+      id: '6peEdPVO73WtgGah5sEhX4',
+      images: [{
+        height: 640,
+        url: 'https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757',
+        width: 640,
+      }, {
+        height: 300,
+        url: 'https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6',
+        width: 300,
+      }, {
+        height: 64,
+        url: 'https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16',
+        width: 64,
+      }],
+      name: 'The Essential Incubus',
+      type: 'album',
+    },
+    {
+      album_type: 'album',
+      artists: [{
+        name: 'Incubus',
+      }],
+      id: '6peEdPVO73WtgGah5sEhX4',
+      images: [{
+        height: 640,
+        url: 'https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757',
+        width: 640,
+      }, {
+        height: 300,
+        url: 'https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6',
+        width: 300,
+      }, {
+        height: 64,
+        url: 'https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16',
+        width: 64,
+      }],
+      name: 'The Essential Incubus',
+      type: 'album',
+    },
+  ];
+
+  const markup = `
+    <div class="list-item" data-album-id="${data[0].id}">
+      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data[0].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data[0].artists[0].name}</p>
+      </div>
+    </div>`;
+
+  const markup2 = `
+    <div class="list-item" data-album-id="${data[0].id}">
+      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data[0].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data[0].artists[0].name}</p>
+      </div>
+    </div>
+    <div class="list-item" data-album-id="${data[0].id}">
+      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data[0].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data[0].artists[0].name}</p>
+      </div>
+    </div>`;
+
+  it('should create and append the markup given a correct data', () => {
+    const element = document.createElement('div');
+    const albumInfoView = new AlbumInfoView(element);
+    albumInfoView.update(data);
+
+    expect(Helper.removeDuplicateSpaces(element.innerHTML))
+      .to.be.eql(Helper.removeDuplicateSpaces(markup));
+  });
+
+  it('should create and append when more than 1 album', () => {
+    const element2 = document.createElement('div');
+    const albumInfoView = new AlbumInfoView(element2);
+    albumInfoView.update(data2);
+
+    expect(Helper.removeDuplicateSpaces(element2.innerHTML))
+      .to.be.eql(Helper.removeDuplicateSpaces(markup2));
+  });
+});
